290fbc707a7a93e4314349875d6f4f3c4a64fb17
PhilBug <filip.bugaj13@gmail.com>
2021-09-17 21:31:55 +0200

    add clinic registration

--- routes/users.js
+++ routes/users.js
@@ -10,4 +10,12 @@ router.get('/patient', patient_controller.register_get);
 /* POST patient form. */
 router.post('/patient', patient_controller.register_post);
 
+var clinic_controller = require('../controllers/clinicController');
+
+/* GET clinic form. */
+router.get('/clinic', clinic_controller.register_get);
+
+/* POST clinic form. */
+router.post('/clinic', clinic_controller.register_post);
+
 module.exports = router;
